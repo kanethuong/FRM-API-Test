a75d8aefe95cff8a24ffb34478f75f5987ce6629
Trần Thiện Phú <hostcode0301@gmail.com>
2021-10-16 19:33:44 +0700

    Fix Controller name to Controller Test [Unit, Intergration]

---
+++ Intergration/Controller/AuthenticationControllerTest/LogoutTest.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace kroniiapiTest.Intergration.Controller.AccountControllerTest
+{
+    public class LogoutTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void LogoutTestTrue()
+        {
+            Assert.Pass();
+        }
+    }
+}
