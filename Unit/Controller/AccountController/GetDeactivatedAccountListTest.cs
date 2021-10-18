c2de161542906c0ae6d5390617348a8411489863
thanhdanh2401 <lephamthanhdanh@gmail.com>
2021-10-18 13:52:14 +0700

    GetDeactivatedAccountList[NunitTest]

--- Unit/Controller/AccountController/GetDeactivatedAccountListTest.cs
+++ Unit/Controller/AccountController/GetDeactivatedAccountListTest.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 
 namespace kroniiapiTest.Unit.Controller.AccountController
 {
-    public class GetDeactivatedAccountListTest
+    public class GetDeactivatedAccountListTest 
     {
-        
+        private readonly Mock<IA> mockIdol = new Mock<IIdolService>();
     }
 }
