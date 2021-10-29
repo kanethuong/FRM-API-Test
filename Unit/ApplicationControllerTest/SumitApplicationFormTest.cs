2eec8888acd0e23c6fa47f25c56e1d55b36b86b9
unknown <lephamthanhdanh@gmail.com>
2021-10-29 14:02:35 +0700

    Update SubmitApplicationFormTest [NUnit]

--- Unit/ApplicationControllerTest/SumitApplicationFormTest.cs
+++ Unit/ApplicationControllerTest/SumitApplicationFormTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
@@ -21,12 +22,12 @@ namespace kroniiapitest.Unit.TraineeControllerTest
     public class SubmitApplicationFormTest
     {
         private IMapper mapper;
-        private readonly Mock<IApplicationService> mockApplicationService;
-        private readonly Mock<ITraineeService> mockTraineeService;
-        private readonly Mock<IMegaHelper> mockMegaHelper;
-        private ApplicationController controller;
-
-        private static IEnumerable<TestCaseData> SubmitApplication
+        private readonly Mock<IMapper> mockMapper = new Mock<IMapper>();
+        private readonly Mock<IApplicationService> mockApplicationService = new Mock<IApplicationService>();
+        private readonly Mock<ITraineeService> mockTraineeService = new Mock<ITraineeService>();
+        private readonly Mock<IMegaHelper> mockMegaHelper = new Mock<IMegaHelper>();
+        
+        private static IEnumerable<TestCaseData> SubmitApplicationTrue
         {
             get
             {
@@ -37,23 +38,119 @@ namespace kroniiapitest.Unit.TraineeControllerTest
                         TraineeId = 1,
                         ApplicationCategoryId = 6
                     },
-                    1,
-                    200
+                    "\\FileForTest\\ApplicationTest.docx",
+                    201
                     );                
             }
         }
 
 
         [Test]
-        [TestCaseSource("SubmitApplication")]
-        public async Task EditProfile_Result(ApplicationInput applicationInput,int result, int statusCode)
+        [TestCaseSource("SubmitApplicationTrue")]
+        public async Task SubmitApplicationTrue_201(ApplicationInput applicationInput,string pathTest, int statusCode)
         {
-            // controller = new ApplicationController(mapper, null, mockApplicationService.Object, null);
-            // mockMegaHelper.Setup(m => m.Upload()); 
-            // mockApplicationService.Setup(tr => tr.InsertNewApplication(It.IsAny<Application>(), It.IsAny<IFormFile>())).ReturnsAsync(result);
-            // var actual = await controller.SubmitApplicationForm(applicationInput, );
-            // var okResult = actual.Result as ObjectResult;
-            // Assert.AreEqual(statusCode, okResult.StatusCode);
+            var config = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new ApplicationProfile());
+            });
+            mapper = config.CreateMapper();
+            ApplicationController applicationController = new ApplicationController(mockMapper.Object, mockTraineeService.Object, mockApplicationService.Object, mockMegaHelper.Object);
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            string pathToTest = projectDirectory + pathTest;
+            var stream = File.OpenRead(pathToTest);
+            IFormFile file = new FormFile(stream, 0, stream.Length, "ApplicationTest", "ApplicationTest.docx");
+            Application application = mapper.Map<Application>(applicationInput);
+            mockMegaHelper.Setup(mega => mega.Upload(stream, "ApplicationTest.docx","ApplicationForm")).ReturnsAsync("url.com");
+            mockApplicationService.Setup(app => app.InsertNewApplication(application, file)).ReturnsAsync(1);            
+            var actual = await applicationController.SubmitApplicationForm(applicationInput, file);
+            var okResult = actual as ObjectResult;
+            Assert.AreEqual(statusCode, okResult.StatusCode);
         }
+
+        private static IEnumerable<TestCaseData> SubmitApplicationNotDocFile
+        {
+            get
+            {
+                //Fail case: File is not .doc or .docx
+                yield return new TestCaseData(
+                    new ApplicationInput
+                    {
+                        Description = "Xac nhan sv",
+                        TraineeId = 1,
+                        ApplicationCategoryId = 6
+                    },
+                    "\\FileForTest\\Avatar.png",
+                    400
+                    );                
+            }
+        }
+
+
+        [Test]
+        [TestCaseSource("SubmitApplicationNotDocFile")]
+        public async Task SubmitApplicationNotDocFile_400(ApplicationInput applicationInput,string pathTest, int statusCode)
+        {
+            var config = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new ApplicationProfile());
+            });
+            mapper = config.CreateMapper();
+            ApplicationController applicationController = new ApplicationController(mockMapper.Object, mockTraineeService.Object, mockApplicationService.Object, mockMegaHelper.Object);
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            string pathToTest = projectDirectory + pathTest;
+            var stream = File.OpenRead(pathToTest);
+            IFormFile file = new FormFile(stream, 0, stream.Length, "ApplicationTest", "ApplicationTest.docx");
+            Application application = mapper.Map<Application>(applicationInput);
+            mockMegaHelper.Setup(mega => mega.Upload(stream, "ApplicationTest.docx","ApplicationForm")).ReturnsAsync("url.com");
+            mockApplicationService.Setup(app => app.InsertNewApplication(application, file)).ReturnsAsync(1);            
+            var actual = await applicationController.SubmitApplicationForm(applicationInput, file);
+            var okResult = actual as ObjectResult;
+            Assert.AreEqual(statusCode, okResult.StatusCode);
+        }
+
+        private static IEnumerable<TestCaseData> SubmitApplicationNoFileSelected
+        {
+            get
+            {
+                //Fail case: File is not .doc or .docx
+                yield return new TestCaseData(
+                    new ApplicationInput
+                    {
+                        Description = "Xac nhan sv",
+                        TraineeId = 1,
+                        ApplicationCategoryId = 6
+                    },
+                    400
+                    );                
+            }
+        }
+
+
+        [Test]
+        [TestCaseSource("SubmitApplicationNoFileSelected")]
+        public async Task SubmitApplicationNoFileSelected_400(ApplicationInput applicationInput,int statusCode)
+        {
+            var config = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new ApplicationProfile());
+            });
+            mapper = config.CreateMapper();
+            ApplicationController applicationController = new ApplicationController(mockMapper.Object, mockTraineeService.Object, mockApplicationService.Object, mockMegaHelper.Object);
+            // string workingDirectory = Environment.CurrentDirectory;
+            // string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            // string pathToTest = projectDirectory + pathTest;
+            // var stream = File.OpenRead(pathToTest);
+            // IFormFile file = new FormFile(stream, 0, stream.Length, "ApplicationTest", "ApplicationTest.docx");
+            Application application = mapper.Map<Application>(applicationInput);
+            // mockMegaHelper.Setup(mega => mega.Upload(stream, "ApplicationTest.docx","ApplicationForm")).ReturnsAsync("url.com");
+            mockApplicationService.Setup(app => app.InsertNewApplication(application, null)).ReturnsAsync(0);            
+            var actual = await applicationController.SubmitApplicationForm(applicationInput, null);
+            var okResult = actual as ObjectResult;
+            Assert.AreEqual(statusCode, okResult.StatusCode);
+        }
+
+
     }
 }
